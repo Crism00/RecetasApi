c9f07cffc4fe1799592f50b82a9289159f2933ad
Cristian <crismoo3012@gmail.com>
2024-11-25 19:13:59 -0600

    [UPD] Responses on recetas

--- app/controllers/categorias_controller.ts
+++ app/controllers/categorias_controller.ts
@@ -1,28 +1,26 @@
 import type { HttpContext } from '@adonisjs/core/http'
 import { storeValidator } from '#validators/categoria'
 import Categoria from '#models/categoria'
+import { standardResponse } from '../helpers/response.js'
 
 export default class CategoriasController {
   async store({ request, response }: HttpContext) {
     const payload = await request.validateUsing(storeValidator)
     const categoria = await Categoria.create(payload)
     if (categoria) {
-      return response.status(200).json({
-        msg: 'Categoria creada correctamente',
-        categoria,
-      })
+      return response
+        .status(200)
+        .json(standardResponse(200, 'Categoria creada correctamente', { categoria }))
     }
 
-    return response.status(500).json({
-      msg: 'Hubo un error al guardar la categoria',
-    })
+    return response.status(500).json(standardResponse(500, 'Hubo un error al guardar la categoria'))
   }
 
   async index({ response }: HttpContext) {
     const categorias = await Categoria.all()
-    return response.status(200).json({
-      categorias,
-    })
+    return response
+      .status(200)
+      .json(standardResponse(200, 'Categorias obtenidas correctamente', { categorias }))
   }
 
   async update({ request, response, params }: HttpContext) {
@@ -30,9 +28,8 @@ export default class CategoriasController {
     const categoria = await Categoria.findOrFail(params.id)
     categoria.merge(payload)
     await categoria.save()
-    return response.status(200).json({
-      msg: 'Categoria actualizada correctamente',
-      categoria,
-    })
+    return response
+      .status(200)
+      .json(standardResponse(200, 'Categoria actualizada correctamente', { categoria }))
   }
 }
--- app/controllers/recetas_controller.ts
+++ app/controllers/recetas_controller.ts
@@ -1,16 +1,16 @@
 import type { HttpContext } from '@adonisjs/core/http'
 import Receta from '#models/receta'
 import { storeValidator } from '#validators/receta'
+import { standardResponse } from '../helpers/response.js'
 
 export default class RecetasController {
   async store({ request, response }: HttpContext) {
     const payload = await request.validateUsing(storeValidator)
     const receta = await Receta.create(payload)
     if (receta) {
-      return response.status(200).json({
-        msg: 'Receta creada correctamente',
-        receta,
-      })
+      return response
+        .status(200)
+        .json(standardResponse(200, 'Receta creada correctamente', { receta }))
     }
 
     return response.status(500).json({
@@ -20,19 +20,20 @@ export default class RecetasController {
 
   async index({ response }: HttpContext) {
     const recetas = await Receta.all()
-    return response.status(200).json({
-      recetas,
-    })
+    return response
+      .status(200)
+      .json(standardResponse(200, 'Recetas obtenidas correctamente', { recetas }))
   }
 
   async update({ request, response, params }: HttpContext) {
     const payload = await request.validateUsing(storeValidator)
     const receta = await Receta.findOrFail(params.id)
     receta.merge(payload)
-    await receta.save()
-    return response.status(200).json({
-      msg: 'Receta actualizada correctamente',
-      receta,
-    })
+    if (await receta.save()) {
+      return response
+        .status(200)
+        .json(standardResponse(200, 'Receta actualizada correctamente', { receta }))
+    }
+    return response.status(500).json(standardResponse(500, 'Hubo un error al actualizar la receta'))
   }
 }
